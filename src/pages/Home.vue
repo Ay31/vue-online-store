f9cc8b0e45cc86e8854c5176fb07df61adf58c8e
Ay31 <yorkyin@welove-inc.com>
2020-07-08 21:03:38 +0800

    feat(vuex): state mutation

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <div class="title">
-      <h1>{{ msg }}</h1>
+      <h1>In Stock</h1>
     </div>
+    <product-list></product-list>
   </div>
 </template>
 
 <script>
+import ProductList from '@/components/products/ProductList.vue'
 export default {
   name: 'home',
   data() {
     return {
       msg: 'Welecome to my vue.js App'
     }
+  },
+  components: {
+    'product-list': ProductList
   }
 }
 </script>
